21319eb0c91d28371195dcd7a4798c385876ffd5
szskill <lumonexu@gmail.com>
2022-03-28 19:40:36 +0800

    Embeds!

--- extensions/moderation.py
+++ extensions/moderation.py
@@ -20,11 +20,22 @@ class Moderation(commands.Cog):
 
         if ctx.author == member:
             return await ctx.reply("😔 I'm too nice to kick you.")
-        elif ctx.author == ctx.bot:
-            return await ctx.reply("🤨 Why?")
+        elif member == ctx.guild.me:
+            return await ctx.reply(
+                embed=disnake.Embed(
+                    title="🤨 Why?",
+                    description="do you got a problem with me?!",
+                    color=disnake.Color.brand_red(),
+                )
+            )
 
         await member.kick(reason=reason)
-        await ctx.reply(f"Kicked `{member}` for `{reason}`")
+        await ctx.reply(
+            embed=disnake.Embed(
+                title=f"✅ Kicked `{member}` for `{reason}`",
+                color=disnake.Color.brand_green(),
+            )
+        )
 
     @commands.command()
     async def ban(
@@ -38,11 +49,22 @@ class Moderation(commands.Cog):
 
         if ctx.author == member:
             return await ctx.reply("😔 I'm too nice to ban you.")
-        elif ctx.author == ctx.bot:
-            return await ctx.reply("🤨 Why?")
+        elif member == ctx.guild.me:
+            return await ctx.reply(
+                embed=disnake.Embed(
+                    title="🤨 Why?",
+                    description="do you got a problem with me?!",
+                    color=disnake.Color.brand_red(),
+                )
+            )
 
         await member.ban(reason=reason)
-        await ctx.reply(f"Banned `{member}` for `{reason}`")
+        await ctx.reply(
+            embed=disnake.Embed(
+                title=f"✅ Banned `{member}` for `{reason}`",
+                color=disnake.Color.brand_green(),
+            ),
+        )
 
 
 def setup(bot: commands.Bot) -> None:
