bcdb7f95497328624cb2a078beab13f45492ca8a
szskill <lumonexu@gmail.com>
2022-03-29 08:45:35 +0800

    Add error handling cog

---
+++ omnia/extensions/error_handling.py
@@ -0,0 +1,27 @@
+import disnake
+from disnake.ext import commands
+
+
+class ErrorHandling(commands.Cog):
+    """The cog that handles errors."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @commands.Cog.listener()
+    async def on_command_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ) -> None:
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.reply(
+                embed=disnake.Embed(
+                    title="Missing Permissions",
+                    description="You do not have permission to perform this command.",
+                    color=disnake.Color.brand_red(),
+                )
+            )
+
+
+def setup(bot: commands.Bot) -> None:
+    """Loads the `ErrorHandling` cog."""
+    bot.add_cog(ErrorHandling())
