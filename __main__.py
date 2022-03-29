29b9029815020f8a694b7c48fb11f2031adfeecb
szskill <lumonexu@gmail.com>
2022-03-29 10:43:04 +0800

    Configure logging

--- __main__.py
+++ __main__.py
@@ -1,9 +1,24 @@
 """Entrypoint for Omnia."""
 
+import logging
 from os import getenv
+
 from dotenv import load_dotenv
+
 from omnia.omnia import Omnia
+
+
+def setup_logging() -> None:
+    """Sets up logging."""
+
+    logging.basicConfig(
+        level=logging.WARN,
+        format="%(asctime)s | %(levelname)s | %(message)s",
+    )
+
 
 if __name__ == "__main__":
     load_dotenv()
+    setup_logging()
+
     Omnia().run(getenv("TOKEN"))
