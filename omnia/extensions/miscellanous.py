c1b490a11368621703c3b3363944b45627975748
szskill <lumonexu@gmail.com>
2022-03-31 14:32:31 +0800

    Add Miscellanous cog

---
+++ omnia/extensions/miscellanous.py
@@ -0,0 +1,32 @@
+import disnake
+from disnake.ext import commands
+
+from omnia.omnia import Omnia
+
+
+class Miscellanous(commands.Cog):
+    """The cog for all things miscellanous."""
+
+    def __init__(self, bot: Omnia) -> None:
+        self.bot = bot
+
+    @commands.command()
+    async def source(self, ctx: commands.Context) -> None:
+        """Shows you the link to the source code of Omnia."""
+
+        await ctx.reply(
+            embed=disnake.Embed(
+                title="Omnia's Source Code",
+                description=(
+                    "Omnia is open source! Check the code out at"
+                    + " <https://github.com/szskill/omnia>! Feel free to contribute or"
+                    + " open issues."
+                ),
+                color=self.bot.primary_color,
+            )
+        )
+
+
+def setup(bot: commands.Bot) -> None:
+    """Loads the `Miscellanous` cog."""
+    bot.add_cog(Miscellanous(bot))
