cb9ba9117b2e6d7e3be21cf0d730f477c563b620
szskill <lumonexu@gmail.com>
2022-04-14 17:29:41 +0800

    Add latency alias to ping and make it show latency

--- omnia/extensions/fun.py
+++ omnia/extensions/fun.py
@@ -1,3 +1,5 @@
+import time
+
 import disnake
 from disnake.ext import commands
 
@@ -10,13 +12,18 @@ class Fun(commands.Cog):
     def __init__(self, bot: Omnia) -> None:
         self.bot = bot
 
-    @commands.command()
+    @commands.command(aliases=["latency"])
     async def ping(self, ctx: commands.Context) -> None:
         """🏓 Pong!"""
 
+        start = time.time_ns()
+        await ctx.trigger_typing()
+        stop = time.time_ns()
+        latency_ms = round((stop - start) / 1_000_000)
+
         embed = disnake.Embed(
             title="🏓 Pong!",
-            description=f"This is Omnia version {self.bot.version}!",
+            description=f"Latency: {latency_ms}ms",
             color=self.bot.primary_color,
         )
 
