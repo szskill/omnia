2c0e472ae9656e36479c66eb6eaba2e5422ce276
szskill <lumonexu@gmail.com>
2022-04-17 16:06:06 +0800

    Make error handling more compact

--- omnia/extensions/error_handling.py
+++ omnia/extensions/error_handling.py
@@ -6,6 +6,30 @@ from disnake.ext import commands
 from ..omnia import Omnia
 from ..fancy_embed import FancyEmbed
 
+ERROR_TITLE_MAP = {
+    "MissingPermissions": "❌ Missing permissions",
+    "BotMissingPermissions": "❌ I'm missing permissions",
+    "MissingRequiredArgument": "❌ Missing required argument",
+    "BadArgument": "❌ Bad argument",
+    "CheckFailure": "❌ Check failed",
+    "CommandOnCooldown": "❌ Command on cooldown",
+    "CommandInvokeError": "❌ Command invoke error",
+    "MemberNotFound": "❌ Member not found",
+    "CommandError": "❌ Unknown error",
+}
+
+ERROR_DESCRIPTION_MAP = {
+    "MissingPermissions": "You're missing permissions to do that.",
+    "BotMissingPermissions": "I'm missing permissions to do that.",
+    "MissingRequiredArgument": "You're missing a required argument.",
+    "commands.BadArgument": "You gave an invalid argument.",
+    "CheckFailure": "You failed a check.",
+    "CommandOnCooldown": "That command is on cooldown.",
+    "CommandInvokeError": "An error occurred while invoking the command.",
+    "MemberNotFound": "I couldn't find that member.",
+    "CommandError": "An unknown error occurred.",
+}
+
 
 class ErrorHandling(commands.Cog):
     """The cog that handles errors."""
@@ -17,48 +41,25 @@ class ErrorHandling(commands.Cog):
     async def on_command_error(
         self, ctx: commands.Context, error: commands.CommandError
     ) -> None:
-        if isinstance(error, commands.MissingPermissions):
-            await ctx.reply(
-                embed=FancyEmbed(
-                    title="❌ Missing permissions",
-                    description="You do not have permission to perform this command.",
-                    color=disnake.Color.brand_red(),
-                )
-            )
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.reply(
-                embed=FancyEmbed(
-                    title="❌ Missing required argument",
-                    description=f"You are missing the `{error.param.name}` argument",
-                    color=disnake.Color.brand_red(),
-                )
-            )
-        elif isinstance(error, commands.MemberNotFound):
-            await ctx.reply(
-                embed=FancyEmbed(
-                    title="❌ Member not found",
-                    description="The member you specified could not be found.",
-                    color=disnake.Color.brand_red(),
-                )
-            )
-        elif isinstance(error, commands.CommandNotFound):
+        if isinstance(error, commands.CommandNotFound):
             return
-        else:
-            if ctx.command:
-                logging.warn(
-                    f"Unhandled error {error.__class__} on command {ctx.command.name}"
-                )
-                await self.bot.redis_db.incr(
-                    f"{self.bot.redis_keyspace}.command_errors.{ctx.command.name}"
-                )
+        elif type(error) not in ERROR_TITLE_MAP.keys() and ctx.command is not None:
+            logging.warn(
+                f"Unhandled error {type(error).__name__} on command {ctx.command.name}"
+            )
+            await self.bot.redis_db.incr(
+                f"{self.bot.redis_keyspace}.command_errors.{ctx.command.name}"
+            )
 
-                await ctx.reply(
-                    embed=FancyEmbed(
-                        title="❌ Unknown error",
-                        description=f"```\n{error}\n```",
-                        color=disnake.Color.brand_red(),
-                    )
-                )
+        await ctx.reply(
+            embed=FancyEmbed(
+                title=ERROR_TITLE_MAP.get(type(error).__name__, "❌ Unknown error"),
+                description=ERROR_DESCRIPTION_MAP.get(
+                    type(error).__name__, f"```\n{error}\n```"
+                ),
+                color=disnake.Color.brand_red(),
+            )
+        )
 
 
 def setup(bot: Omnia) -> None:
