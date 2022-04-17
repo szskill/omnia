c72f09bb1d9c35c0beb04b2099edf953b33d9ffb
szskill <lumonexu@gmail.com>
2022-04-17 18:18:46 +0800

    Make FancyEmbed fancier

--- omnia/fancy_embed.py
+++ omnia/fancy_embed.py
@@ -3,6 +3,8 @@ import datetime
 import disnake
 from disnake.ext.commands import Context
 
+from .omnia import Omnia
+
 
 class FancyEmbed(disnake.Embed):
     def __init__(self, *args, ctx: Context | None = None, **kwargs):
@@ -12,5 +14,11 @@ class FancyEmbed(disnake.Embed):
             self.set_author(
                 name=ctx.author.display_name, icon_url=ctx.author.display_avatar.url
             )
+
+            if isinstance(ctx.bot, Omnia) and ctx.bot.user.avatar is not None:
+                self.set_footer(
+                    text=f"Omnia {ctx.bot.version}",
+                    icon_url=ctx.bot.user.avatar.url,
+                )
 
         super().__init__(*args, **kwargs)
