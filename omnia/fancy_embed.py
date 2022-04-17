2e1328c81c1a9392981d42fafb9ce7bc2b7d2905
szskill <lumonexu@gmail.com>
2022-04-17 18:10:34 +0800

    Move super().__init__() after FancyEmbed customizations

--- omnia/fancy_embed.py
+++ omnia/fancy_embed.py
@@ -6,11 +6,11 @@ from disnake.ext.commands import Context
 
 class FancyEmbed(disnake.Embed):
     def __init__(self, *args, ctx: Context | None = None, **kwargs):
-        super().__init__(*args, **kwargs)
-
         self.timestamp = datetime.datetime.now()
 
         if ctx is not None:
             self.set_author(
                 name=ctx.author.display_name, icon_url=ctx.author.display_avatar.url
             )
+
+        super().__init__(*args, **kwargs)
