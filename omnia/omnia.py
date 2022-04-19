86f186b84ad741bb95b705f17961b9f7e547163c
szskill <lumonexu@gmail.com>
2022-04-19 08:56:47 +0800

    Add Jishaku support

--- omnia/omnia.py
+++ omnia/omnia.py
@@ -21,6 +21,7 @@ class Omnia(commands.Bot):
         self.redis_port = 0
         self.use_fake_redis = False
         self.primary_color = disnake.Color(0x000000)
+        self.enable_jishaku = False
 
         self.redis_db: aioredis.Redis = None  # type: ignore
         self.redis_keyspace: str = "omnia"
@@ -29,19 +30,26 @@ class Omnia(commands.Bot):
         self._register_extensions()
         self._connect_to_databases()
 
+        if self.enable_jishaku:
+            try:
+                self.load_extension("jishaku")
+            except ImportError:
+                print("disnake-jishaku is not installed.")
+
     def _read_from_config_file(self, file: str = "config.yaml") -> None:
         """Reads configuration from a `config.yaml` file."""
 
         with open(file) as handle:
             config: dict = yaml.load(handle, yaml.FullLoader)
 
-            self.command_prefix = config.get("prefix", "o::")
+            self.command_prefix = config.get("prefix", "'")
             self.version = config.get("version", "0.0.1")
             self.statuses = config["statuses"]
             self.enabled_extensions = config["enabled-extensions"]
             self.redis_port = config["redis"].get("port", 6379)
             self.use_fake_redis = config["redis"].get("fake", True)
             self.primary_color = disnake.Color(int(config["primary-color"]))
+            self.enable_jishaku = config["enable-jishaku"]
 
     def _register_extensions(self) -> None:
         """Registers all extensions from `self.enabled_extensions`."""
